51cfb8d76403f22769a086884153afa68cc2d526
leorjorge <leonardorejorge@gmail.com>
2025-01-23 15:03:12 +0100

    Fix the calculation of confidence intervals
    Replaces the method of calculation of confidence intervals. Instead of the 2*standard error, which does not work for randomizations and assumes a normal distribution of the randomized values, this change uses the quantiles of the randomized distribution directly.

--- Functions/IC.R
+++ Functions/IC.R
@@ -2,9 +2,8 @@
 IC <- function(X, conf.level = 0.95){
   m <- mean(X)
   s <- sd(X)
-  n <- length(X)
-  se <- s/sqrt(n)
-  error <- qt((1-conf.level)/2, df = n-1, lower.tail = FALSE)*se
-  res <- c(mean = m, sd = s, lower = m-error, upper = m+error)
+  tails <- (1 - conf.level)/2
+  ic <- as.numeric(quantile(X, probs = c(tails, 1 - tails)))
+  res <- c(mean = m, sd = s, lower = ic[1], upper = ic[2])
   return(res)
 }
